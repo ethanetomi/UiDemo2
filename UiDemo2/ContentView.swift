1f7363dc283de17e4a59435ac73b5198d070d610
Ethan Etomi <ethanetomi@icloud.com>
2023-10-29 14:19:24 -0400

    Added Spacing and Alignment

--- UiDemo2/ContentView.swift
+++ UiDemo2/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Image("Vegas")
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+            Text("Las Vegas Eiffel Tower")
+            Text("Come visit the Las Vegas Eiffel Tower for fun and games.")
+            
         }
-        .padding()
+        
     }
 }
 
