d11dcfdf779719f46a950bb9c57980be71b0175c
Ethan Etomi <ethanetomi@icloud.com>
2023-10-29 15:12:27 -0400

    Finished Product

--- UiDemo2/ContentView.swift
+++ UiDemo2/ContentView.swift
@@ -9,52 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20.0){
-            Image("Vegas")
-                .resizable()
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
-                .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
-            HStack {
-                Text("Las Vegas Eiffel Tower")
-                    .font(.title)
-                    .fontWeight(.bold)
-                Spacer()
-                VStack{
-                    HStack{
-                        Image(systemName: "star.fill")
+        ZStack{
+            Color(.systemBrown)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0){
+                Image("Vegas")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Text("Las Vegas Eiffel Tower")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                                
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.fill")
                             
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.leadinghalf.fill")
+                        }
                         
+                        Text("(Reviews  4024)")
                     }
-                    
-                    Text("(Reviews  4024)")
+                    .foregroundColor(.yellow)
+                    .font(.caption)
                 }
-                .foregroundColor(.yellow)
+                
+                
+                Text("Come visit the Las Vegas Eiffel Tower for fun and games.")
+                HStack{
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.gray)
                 .font(.caption)
+                
+                
             }
-            
-            
-            Text("Come visit the Las Vegas Eiffel Tower for fun and games.")
-            HStack{
-                Spacer()
-                Image(systemName: "binoculars.fill")
-                Image(systemName: "fork.knife")
-            }
-            .foregroundColor(.gray)
-            .font(.caption)
-            
-            
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.gray)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+            )
+            .padding()
         }
-        .padding()
-        .background(Rectangle()
-            .foregroundColor(.gray)
-            .cornerRadius(15)
-            .shadow(radius: 15)
-        )
-        .padding()
+        
         
     }
 }
