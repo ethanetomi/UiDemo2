ecf3f4075f19dbb3eeb44c949cbac9a176fc2b2d
Ethan Etomi <ethanetomi@icloud.com>
2023-10-29 14:29:57 -0400

    Added horizontal stack with 4 and a half star rating

--- UiDemo2/ContentView.swift
+++ UiDemo2/ContentView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
+        VStack(alignment: .leading, spacing: 20.0){
             Image("Vegas")
                 .resizable()
                 .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
                 .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
-            Text("Las Vegas Eiffel Tower")
+            HStack {
+                Text("Las Vegas Eiffel Tower")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.leadinghalf.fill")
+            }
+            
             Text("Come visit the Las Vegas Eiffel Tower for fun and games.")
             
         }
