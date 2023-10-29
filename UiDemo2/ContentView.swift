521c2691c4f3feb28e2dfa297d0b88eafdb248a7
Ethan Etomi <ethanetomi@icloud.com>
2023-10-29 14:02:05 -0400

    Initial Commit

---
+++ UiDemo2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UiDemo2
+//
+//  Created by Ethan Etomi on 10/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UiDemo2/UiDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  UiDemo2App.swift
+//  UiDemo2
+//
+//  Created by Ethan Etomi on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct UiDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
